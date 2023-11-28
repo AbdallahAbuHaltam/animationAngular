a180e2e59501b30e4f12c606f6752c8cfdd2c465
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-28 11:54:59 +0300

    Using  Void

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,28 @@ import { Component } from '@angular/core';
       ]),
 
 
-    ])
+    ]),
+    trigger('list1',[
+      state('in',style({
+        opacity:1,
+        transform:'translateX(0)'
+      })),
+      
+      transition('void => *',[
+        style({
+          opacity:0,
+          transform:'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void',[
+        animate(300,style({
+          transform:'translateX(100px)',
+          opacity:0
+        }))
+      ]),
+
+    ]),
   ]
 })
 export class AppComponent {
@@ -59,6 +80,9 @@ export class AppComponent {
     }
     onShrink(){
       this.wildState='shrunken';
+
+    }
+    onDelete(item){
 
     }
 
